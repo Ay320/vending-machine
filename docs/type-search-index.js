2930ced68ada249a60ba9bfcc8854281570b4722
Ayham Ahmed <163321889+Ay320@users.noreply.github.com>
2024-10-14 20:35:46 +0100

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"CassetteException"},{"p":"<Unnamed>","l":"InvalidPasswordException"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Press"},{"p":"<Unnamed>","l":"VendingMachine"}];updateSearchResults();
